c9ca1357de87b1dbd6caded24d0f5c34eeff62a5
Md-usman-05 <mdusmanmd2005@gmail.com>
2026-05-11 23:29:52 +0530

    Completed landing, login and register frontend UI

--- summarizer/views.py
+++ summarizer/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return render(request,"summarizer/index.html")
+    return render(request,'summarizer/index.html')
+
+def login_view(request):
+    return render(request,'summarizer/login.html')
+def register_view(request):
+    return render(request,'summarizer/register.html')
+def upload_view(request):
+    return render(request,'summarizer/upload.html')
