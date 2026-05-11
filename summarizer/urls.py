a6501faa7c49dbb974936059834ad024177d7689
Md-usman-05 <mdusmanmd2005@gmail.com>
2026-05-11 23:29:52 +0530

    Completed landing, login and register frontend UI

--- summarizer/urls.py
+++ summarizer/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login/',views.login_view,name='login'),
+    path('register/',views.register_view,name='register'),
+    path('upload/',views.upload_view,name='upload'),
 ]
