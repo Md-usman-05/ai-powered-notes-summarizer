80e18b56140c4d9a593ddf9c7f3b0f7b4f536761
Md-usman-05 <mdusmanmd2005@gmail.com>
2026-05-12 10:25:44 +0530

    Added gitignore

--- summarizer/views.py
+++ summarizer/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'summarizer/index.html')
-
 def login_view(request):
     return render(request,'summarizer/login.html')
 def register_view(request):
